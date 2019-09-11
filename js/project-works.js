51892674d88be796f9e3a3cdd5de7baf05458ff9
Mark <tractor2554393@yandex.by>
2019-09-12 00:13:33 +0300

    add project works page

---
+++ js/project-works.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    $('.comments__slider').slick({
+        slide: '.comments__slider__item',
+        arrows: true,
+        speed: 500,
+        slidesToShow: 1
+    });
+}
